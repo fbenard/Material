82bd503a49ee144f948b487489dbfb2d46f8c5fd
Fabien Bénard <fb.spam@me.com>
2014-12-29 00:16:00 +0100

    Adds empty tests + CircleCI support

---
+++ Tests/Classes/QueryTest.php
@@ -0,0 +1,35 @@
+<?php
+
+// Namespace
+
+namespace Material\Tests\Classes;
+
+
+/**
+ *
+ */
+
+class QueryTest
+extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 *
+	 */
+
+	public function test_get()
+	{
+		$this->markTestIncomplete();
+	}
+
+
+	/**
+	 *
+	 */
+
+	public function test_render()
+	{
+		$this->markTestIncomplete();
+	}
+}
+
+?>
