e20d450292978e7d630b4820e1c28bd24c976ca5
Fabien Bénard <fb.spam@me.com>
2015-02-12 12:42:09 +0100

    Move code under Application

---
+++ Application/Services/Factories/QueryFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+// Namespace
+
+namespace fbenard\Material\Services\Factories;
+
+
+/**
+ *
+ */
+
+class QueryFactory
+{
+	/**
+	 *
+	 */
+
+	public function buildQuery()
+	{
+		// Build a query
+
+		$query = new \fbenard\Material\Classes\Query();
+
+
+		return $query;
+	}
+}
+
+?>
