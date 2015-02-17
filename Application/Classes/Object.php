71351764fabcc9348cf231fea8185f02b246dd87
Fabien <Fabien@splio-fabien-imac.local>
2015-02-17 11:29:12 +0100

    AbstractModel becomes object

---
+++ Application/Classes/Object.php
@@ -0,0 +1,76 @@
+<?php
+
+// Namespace
+
+namespace Splio\Goloboard\Classes;
+
+
+/**
+ *
+ */
+
+class Object
+{
+	// Attributes
+
+	private $_properties = null;
+
+	
+	/*
+	public function load($id);
+	public function delete();
+	public function count();
+	public function save();
+	public function destroy($ids);
+	*/
+
+	/**
+	 *
+	 */
+
+	public function __construct($modelCode)
+	{
+		/*
+		Must build core properties
+		As well as properties defined in the model
+		*/
+	}
+
+
+	/**
+	 *
+	 */
+
+	public function load($id)
+	{
+	}
+
+	
+	/**
+	 *
+	 */
+
+	public function loadFromInitialObject($initialObject)
+	{
+	}
+
+
+	/**
+	 *
+	 */
+
+	public function loadFromRawObject($rawObject)
+	{
+	}
+
+
+	/**
+	 *
+	 */
+
+	public function save()
+	{
+	}
+}
+
+?>
