c1ca440d09243040db589588aa67d1a65405a473
Fabien Bénard <fb.spam@me.com>
2015-03-10 22:15:01 +0100

    Removes events

--- Application/Constants/Events.php
+++ Application/Constants/Events.php
@@ -5,19 +5,4 @@
 \z\cons('EVENT_OBJECT_SAVE_PRE');
 \z\cons('EVENT_OBJECT_SAVE_POST');
 
-
-// Models
-
-\z\cons('EXCEPTION_MODEL_NOT_FOUND');
-
-
-// Objects
-
-\z\cons('EXCEPTION_OBJECT_PROPERTY_NOT_FOUND');
-
-
-// Queries
-
-\z\cons('EXCEPTION_DB_QUERY_FAILED');
-
 ?>
