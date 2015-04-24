020693a941c7b6f3f8e52c98eb25fd660c8d579c
Fabien Bénard <fb.spam@me.com>
2015-04-24 20:54:13 +0200

    Adds method to delete an index

--- Application/Services/Managers/IndexManager.php
+++ Application/Services/Managers/IndexManager.php
@@ -17,17 +17,29 @@ class IndexManager
 
 	public function createIndex($indexCode, $index)
 	{
+		// Delete the index
+
+		$this->deleteIndex($indexCode);
+
+
+		// Create the index
+
+		\z\service('driver/db/es')->indices()->create($index);
+	}
+
+
+	/**
+	 *
+	 */
+
+	public function deleteIndex($indexCode)
+	{
 		// Delete existing index
 
 		if (\z\service('driver/db/es')->indices()->exists(['index' => $indexCode]) === true)
 		{
 			\z\service('driver/db/es')->indices()->delete(['index' => $indexCode]);
 		}
-
-
-		// Create the index
-
-		\z\service('driver/db/es')->indices()->create($index);
 	}
 
 
