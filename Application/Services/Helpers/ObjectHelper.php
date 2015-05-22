b88bc7cede3d7421b58733574eab397a610114d1
Fabien Bénard <fbenard.public@gmail.com>
2015-05-22 17:06:53 +0200

    Adds an object helper

---
+++ Application/Services/Helpers/ObjectHelper.php
@@ -0,0 +1,23 @@
+<?php
+
+// Namespace
+
+namespace fbenard\Material\Services\Helpers;
+
+
+/**
+ *
+ */
+
+class ObjectHelper
+{
+	/**
+	 *
+	 */
+
+	public function isPropertyRelation($property)
+	{
+	}
+}
+
+?>
