fa74b9c82469c6a2bb868241741ec429fa0cb6cb
Fabien Bénard <fbenard@users.noreply.github.com>
2015-09-03 18:43:37 +0200

    Adds method isLocalized

--- Application/Services/Helpers/ObjectPropertyHelper.php
+++ Application/Services/Helpers/ObjectPropertyHelper.php
@@ -15,6 +15,27 @@ class ObjectPropertyHelper
 	 *
 	 */
 
+	public function isLocalized($property)
+	{
+		if
+		(
+			(isset($property['localized']) === true) &&
+			($property['localized'] === true)
+		)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+
+	/**
+	 *
+	 */
+
 	public function isRelation($property)
 	{
 		if (strpos($property['type'], 'model:') === 0)
