868d53afa54ac3eb5b721acc5aeb43f81c70840b
Fabien Bénard <fbenard.public@gmail.com>
2015-12-24 11:15:25 +0100

    Adds relation tests

--- App/Services/Helpers/ObjectPropertyHelper.php
+++ App/Services/Helpers/ObjectPropertyHelper.php
@@ -36,6 +36,78 @@ class ObjectPropertyHelper
 	 *
 	 */
 
+	public function is01Relation($property)
+	{
+		if
+		(
+			($this->isRelation($property) === true) &&
+			(array_key_exists('cardinality', $property) === true) &&
+			(
+				($property['cardinality'] === '0_1') ||
+				($property['cardinality'] === '1_1')
+			)
+		)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+
+	/**
+	 *
+	 */
+
+	public function is0NRelation($property)
+	{
+		if
+		(
+			($this->isRelation($property) === true) &&
+			(array_key_exists('cardinality', $property) === true) &&
+			(
+				($property['cardinality'] === '0_n') ||
+				($property['cardinality'] === '1_n')
+			)
+		)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+
+	/**
+	 *
+	 */
+
+	public function isPivotRelation($property)
+	{
+		if
+		(
+			($this->is0NRelation($property) === true) &&
+			(array_key_exists('pivot', $property) === true) &&
+			(empty($property['pivot']) === false)
+		)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+
+	/**
+	 *
+	 */
+
 	public function isRelation($property)
 	{
 		if
